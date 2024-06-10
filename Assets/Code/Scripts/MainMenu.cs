6fe6550fc724c7f6adfcb389b952f939ba94863c
Ale <alejandro.wenzler@gmail.com>
2024-06-10 21:58:12 +0200

    expanded Main menu

---
+++ Assets/Code/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        int buildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadSceneAsync(buildIndex + 1);
+        SceneManager.LoadSceneAsync(buildIndex + 2, LoadSceneMode.Additive);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
