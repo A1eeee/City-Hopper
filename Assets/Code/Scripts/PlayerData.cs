a092131d0c53799c5fa58db959d389d80393a19e
Ale <alejandro.wenzler@gmail.com>
2024-06-16 22:58:47 +0200

    Added Save and Load system

---
+++ Assets/Code/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float[] position;
+
+    public PlayerData(PlayerMovement playerMovement)
+    {
+        position = new float[3];
+        position[0] = playerMovement.transform.position.x;
+        position[1] = playerMovement.transform.position.y;
+        position[2] = playerMovement.transform.position.z;
+    }
+}
